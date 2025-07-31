7e738050560cde3d687e6b6dbbe1b2960b7d3dd0
MyWayOrHightWay <nfz8723@gmail.com>
2025-07-31 17:48:07 +0500

    haeder have been added

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
-
 const Header = () => {
-	return <div>Header</div>
+	return (
+		<>
+			<header>
+				<h1>Welcome to Our Application</h1>
+				<nav>
+					<ul>
+						<li>
+							<a href='/'>Home</a>
+						</li>
+						<li>
+							<a href='/about'>About</a>
+						</li>
+						<li>
+							<a href='/contact'>Contact</a>
+						</li>
+					</ul>
+				</nav>
+				<div className='user-info'>
+					<p>
+						Logged in as: <strong>User123</strong>
+					</p>
+					<button>Logout</button>
+				</div>
+			</header>
+		</>
+	)
 }
 
 export default Header
